3a9a3b92569c282dc21d48b5f48387631af7439d
gfedorov7 <fedorov.programming@gmail.com>
2026-02-05 14:13:51 +0300

    solve task1

---
+++ task1.cpp
@@ -0,0 +1,21 @@
+﻿#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            int value = target-nums[i];
+            if (map.contains(value))
+                return {map[value], i};
+
+            map[nums[i]] = i;
+        }
+        return {};
+    }
+};
