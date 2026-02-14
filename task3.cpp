5d74bf2d15ab6b7e3e38eadea3102cf971444cc9
gfedorov7 <fedorov.programming@gmail.com>
2026-02-14 23:59:31 +0300

    task3 leetcode

--- task3.cpp
+++ task3.cpp
@@ -7,22 +7,18 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int mx = 0;
-        if (s == "") return 0;
-        if (s.length() == 1) return 1;
+        int left = 0, right = 0, maxLen = 0;
+        map<char, int> lastIndex;
 
-        for(int i = 0; i < s.length() - 1; i++)
-        {
-            map<char, bool> check;
-            check[s[i]] = true;
-            for(int j = i+1; j < s.length(); j++)
-            {
-                if (check[s[j]]) break;
-                check[s[j]] = true;
+        while(right < s.length()) {
+            if (lastIndex.find(s[right]) != lastIndex.end()) {
+                left = max(left, lastIndex[s[right]] + 1);
             }
-            if (check.size() > mx) mx = check.size();
+            lastIndex[s[right]] = right;
+            maxLen = max(maxLen, right - left + 1);
+            right++;
         }
 
-        return mx;
+        return maxLen;
     }
 };
