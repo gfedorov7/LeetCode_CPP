99c5f5f8d3401073f8bab815dab6c3a7a9a3530e
gfedorov7 <fedorov.programming@gmail.com>
2026-02-14 23:35:05 +0300

    task2 leetcode

---
+++ task2.cpp
@@ -0,0 +1,47 @@
+#include "iostream"
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+/* *
+ * Input: l1 = [2,4,3], l2 = [5,6,4]
+ * Output: [7,0,8]
+ * Explanation: 342 + 465 = 807.
+ * [7,0,8]
+ */
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* head = new ListNode{0};
+        ListNode* current = head;
+        int carry = 0;
+
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            int sum = carry;
+
+            if (l1 != nullptr) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+
+            if (l2 != nullptr) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            carry = sum / 10;
+            current->next = new ListNode{sum % 10};
+            current = current->next;
+        }
+
+        ListNode* result = head->next;
+        delete head;
+        return result;
+    }
+};
