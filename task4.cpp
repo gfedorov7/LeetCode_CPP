d0ce70e8fb555f1d15f2153b1bfca74a97502573
gfedorov7 <fedorov.programming@gmail.com>
2026-02-15 00:09:38 +0300

    task4 leetcode

---
+++ task4.cpp
@@ -0,0 +1,18 @@
+#include "string"
+#include "vector"
+#include "algorithm"
+
+using namespace std;
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        nums1.insert(nums1.end(), nums2.begin(), nums2.end());
+        sort(nums1.begin(), nums1.end());
+
+        if (nums1.size() % 2 != 0)
+            return nums1[nums1.size() / 2];
+        else
+            return (double)(nums1[nums1.size() / 2] + nums1[nums1.size() / 2 - 1]) / 2.0;
+    }
+};
